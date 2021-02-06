352a65cb56474964c14d6999cefcc64a1a6d44e1
Anthony Hornung <anthony@hornung.co.uk>
2021-02-06 20:18:10 +0100

    Created Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating sample users'
+10.times do
+  User.new(
+    email: Faker::Internet.email,
+    password: 'rentmygarden',
+    )
+end
+
+puts 'Creating sample gardens'
+counter = 1
+100.times do
+  Garden.new(
+    name: "Garden #{counter}",
+    description: Faker::Lorem.paragraph,
+    location: Faker::Address.full_address,
+    price: rand(10..500),
+    )
+  counter += 1
+end
+
+puts "Seed complete"
