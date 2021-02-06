ed109a5940887c2de5d8e2c0361508979297e21b
Anthony Hornung <anthony@hornung.co.uk>
2021-02-06 16:20:09 +0100

    Updated the gardens controller

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,2 +1,44 @@
 class GardensController < ApplicationController
+  before_action :set_cocktail, only: [:show]
+  def index
+    @gardens = Garden.all
+  end
+
+  def show
+  end
+
+  def new
+    @garden = Garden.new
+  end
+
+  def create
+    @garden = Garden.new(garden_params)
+    if @garden.save
+      redirect_to garden_path(@garden)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    @garden.update(garden_params)
+    redirect_to gardens_path
+  end
+
+  def destroy
+    @garden.destroy
+    redirect_to gardens_path
+  end
+
+  private
+
+  def set_garden
+    @garden = Garden.find(params[:id])
+  end
+
+  def garden_params
+    params.require(:garden).permit(:description, :location, :price)
+  end
 end
