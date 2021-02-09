0052e09d6e66dd511969fd760fdc807079cf8660
Anthony Hornung <anthony@hornung.co.uk>
2021-02-09 22:43:43 +0100

    added edit & update methods to the bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,18 @@
 class BookingsController < ApplicationController
+  def edit; end
+
+  def update
+    @booking.update(booking_params)
+    redirect_to booking_path
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :confirmed)
+  end
 end
